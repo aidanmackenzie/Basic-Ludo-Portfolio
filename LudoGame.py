186517ed3e186917d7247e5214c5978563564c1a
aidanmackenzie <mackenai@oregonstate.edu>
2022-07-31 19:52:44 -0400

    This is looking like a doozy.

---
+++ LudoGame.py
@@ -0,0 +1,8 @@
+# Author: Aidan MacKenzie
+# GitHub username: aidanmackenzie
+# Date: 07/31/2022
+# Description: This code consists of a simplified game of Ludo. This game is made up of classes LudoGame and Player at
+# a minimum. The player class contains at least the position the player chooses, the start and end space for the player,
+# current position of their two tokens, and the current state of the player. It may contain additional information.
+# Whereas the LudoGame object contains information about the players and information about the board.
+
